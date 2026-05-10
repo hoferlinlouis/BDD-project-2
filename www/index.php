73de080926b18fd2504b0dd41ee69c3509daca89
lynbhl <110718551+lynbhl@users.noreply.github.com>
2026-05-10 17:10:17 +0200

    Rename 'CD manager' link to 'CD management'

--- www/index.php
+++ www/index.php
@@ -12,7 +12,7 @@
         <li><a href="search_client.php">Search for a client</a></li>
         <li><a href="search_event.php">Search for an event</a></li>
         <li><a href="search_location.php">Search for a location</a></li>
-        <li><a href="cd_management.php">CD manager</a></li>
+        <li><a href="cd_management.php">CD management</a></li>
         <li><a href="cd_dashboard.php">CD dashboard</a></li>
         <li><a href="cd_availability_dashboard.php">CD availability dashboard</a></li>
         <li><a href="event_board.php">Event dashboard</a></li>
